e6eadd696c682d4b0adfaddcac6f116f2534893e
anilkumarti <anil.tiwari1403@gmail.com>
2024-04-09 16:13:03 +0530

    add button added

--- src/compoments/Meals/MealItem/MealItem.js
+++ src/compoments/Meals/MealItem/MealItem.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React,{useState} from 'react';
 import classes from './MealItem.module.css'
 const MealItem = (props) => {
+  const [currentMeal,changeCurrentMeal]=useState(0);
+  const handleMeal=(e)=> {
+changeCurrentMeal(currentMeal+1)
+  }
     const price=`$${props.price}`
   return (
     <li className={classes.meal}>
@@ -8,7 +12,12 @@ const MealItem = (props) => {
     <div className={classes.description}> {props.description}</div>
     <div className={classes.price}> {price}</div>
     </div>
-    <div> extra div</div>
+    <div> 
+       Amount={currentMeal}
+       <div> <button onClick={handleMeal}> + Add</button></div>
+       
+
+    </div>
     </li>
   )
 }
